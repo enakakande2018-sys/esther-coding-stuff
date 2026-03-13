253b2bf4dc87bb0222ec35fd38f53d3c6b491b95
Esther Nakakande <enakakande2018@gmail.com>
2026-03-13 11:45:12 +0300

    Initial project commit

---
+++ script.js
@@ -0,0 +1,127 @@
+
+const studentName = "Nakakande Esther Enid"
+const date = "March 12, 2026";
+console.log("Student: " + studentName);
+console.log("Date: " + date);
+console.log("--------------------------");
+
+
+
+
+let name = "Esther"
+console.log(name);
+
+
+let age = 20; 
+console.log("I am " + age + " years old.");
+
+
+let a = 10;
+let b = 5;
+console.log(a + b);
+let school="SMASK SCHOOL";
+console.log("I Study at"+school+".");
+
+let city="Kampala"
+let country="Uganda"
+let continent="Africa"
+console.log("I live in"+city+","+country+","+continent+".");
+
+// 🟢 PART 2: WORKING WITH NUMBERS
+
+// 1. Multiplication
+let num1 = 10;
+let num2 = 4;
+console.log(num1 * num2);
+
+// 2. Total Cost
+let price = 50;
+let quantity = 5;
+let total = price * quantity;
+console.log("Total = " + total);
+
+// 3. Temperature Logic
+let temperature = 35; 
+if (temperature > 30) {
+    console.log("It is hot today");
+} else {
+    console.log("The weather is normal");
+}
+
+// 4. Pass/Fail
+let score = 75; 
+if (score > 50) {
+    console.log("Pass");
+} else {
+    console.log("Fail");
+}
+
+// 5. Rectangle Area
+let length = 10;
+let width = 5;
+let area = length * width;
+console.log("Area of rectangle: " + area);
+
+// 🔵 PART 3: STRINGS AND COMBINING VARIABLES
+
+// 1. Full Name
+let firstName = "Esther";
+let lastName = "Enid";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// 2. Favorite Color
+let favoriteColor = "blue";
+console.log("My favorite color is " + favoriteColor);
+
+// 3. Updating Values
+let message = "Hello students";
+message = "Welcome to JavaScript class";
+console.log(message);
+
+// 4. Date Formatting
+let day = 12;
+let month = "March";
+console.log("Today is " + day + " " + month);
+
+// 1. The Swap
+let x = 10;
+let y = 20;
+let temp = x;
+x = y;
+y = temp;
+console.log("x is now " + x + ", y is now " + y);
+
+// 2. Booleans (True or False)
+let isStudent = true;
+console.log("Am I a student? " + isStudent);
+
+// 3. Grading System (Multiple Conditions)
+let marks = 85; 
+if (marks > 80) {
+    console.log("Excellent");
+} else if (marks > 60) {
+    console.log("Good");
+} else {
+    console.log("Try Again");
+}
+
+// 4. Circle Area
+let radius = 7;
+let circleArea = 3.14 * (radius * radius); 
+console.log("Circle Area: " + circleArea);
+
+// 5. Inventory
+let productName = "Laptop";
+let productPrice = 1200;
+console.log("The product " + productName + " costs " + productPrice + " dollars.");
+
+// 6. Mini Challenge: Bio
+console.log("Intro: My name is " + fullName + ", I am " + age + " years old, and I live in " + country + ".");
+
+
+
+
+
+
+
